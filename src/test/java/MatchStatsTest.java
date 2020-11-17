958c902389156c3f9b1ded4cacc0db98af2f474f
evalalos <crowdtestingmilos@gmail.com>
2020-11-17 21:07:57 +0100

    - added new action items

--- src/test/java/MatchStatsTest.java
+++ src/test/java/MatchStatsTest.java
@@ -21,7 +21,7 @@ public class MatchStatsTest extends Base {
 
 		Receiver receiver = new Receiver();
 
-		receiver.collectMatch(Games.UNDER_OR_EQUAL_2);
+		receiver.collectMatch(Games.BOTH_TO_SCORE_AND_OVER_OR_EQUAL_3);
 	}
 
 	@After
