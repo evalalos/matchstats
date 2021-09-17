665b281e148232e6f824a2a4723dbcf3848fead2
milos <milosnkg@gmail.com>
2021-09-17 16:55:50 +0200

    - added new functionalities

--- src/main/java/common/Receiver.java
+++ src/main/java/common/Receiver.java
@@ -16,80 +16,37 @@ public class Receiver extends Base {
     GameCalculator gameCalculator = new GameCalculator();
 
     public Receiver acceptCookies() throws InterruptedException {
-
         Thread.sleep(2000);
         getDriver().findElement(Locators.acceptCookies).click();
-
         return this;
     }
 
     public boolean verifyNoMutualMatchesIsNotVisible() {
-
         driver.manage().timeouts().implicitlyWait(0, TimeUnit.MILLISECONDS);
         boolean exists = driver.findElements(Locators.noMutualGames).size() != 0;
         driver.manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS);
-
         return exists;
     }
 
     private Receiver goToHeadToHeadTab() {
-
         waitForElementToBeClickable(Locators.headToHeadTab);
         getDriver().findElement(Locators.headToHeadTab).click();
-
         return this;
     }
 
     private void getLastFiveResultsAndCollectFullTime(Game game, int includeCriteria) throws IOException {
-
         if (!verifyNoMutualMatchesIsNotVisible()) {
             gameCalculator.calculateFullTime(game, includeCriteria);
         }
     }
 
-    private void getLastFiveResultsAndCollectHalfTime(Game game, int includeCriteria) throws IOException, InterruptedException {
-
+    private void getLastFiveResultsAndCollectHalfTime(Game game, int includeCriteria) throws InterruptedException, IOException {
         if (!verifyNoMutualMatchesIsNotVisible()) {
             gameCalculator.calculateHalfTime(game, includeCriteria);
         }
     }
 
-    public void collectFullTimeMatch(Game game, int includeCriteria) throws IOException, InterruptedException {
-
-        acceptCookies();
-        List<WebElement> allMatches = getDriver().findElements(Locators.unplayedMatches);
-
-        for (WebElement all : allMatches) {
-            all.click();
-            switchToWindow();
-            goToHeadToHeadTab();
-            getLastFiveResultsAndCollectFullTime(game, includeCriteria);
-            switchTab();
-        }
-        sortList(game, includeCriteria);
-    }
-
-    public void collectHalfTimeMatch(Game game, int includeCriteria) throws IOException, InterruptedException {
-
-        acceptCookies();
-        List<WebElement> allMatches = getDriver().findElements(Locators.unplayedMatches);
-
-        for (WebElement all : allMatches) {
-            all.click();
-            switchToWindow();
-            Thread.sleep(500);
-            goToHeadToHeadTab();
-            Thread.sleep(500);
-            getLastFiveResultsAndCollectHalfTime(game, includeCriteria);
-            Thread.sleep(2000);
-            switchTab();
-            Thread.sleep(1000);
-        }
-        sortList(game, includeCriteria);
-    }
-
     public void sortList(Game game, int includeCriteria) {
-
         try (Stream<String> lines = Files.lines(Paths.get(FileName.MATCHES_UNSORTED(game, includeCriteria)))) {
             int n = 0;
             List<String> sortedLines = lines.sorted(Comparator.comparing(line -> line.substring(n)))
--- src/test/java/MatchStatsTest.java
+++ src/test/java/MatchStatsTest.java
@@ -10,20 +10,21 @@ import java.io.IOException;
 
 public class MatchStatsTest extends Base {
 
-	@Before public void setUp() {
-		createChromeDriver();
-		openUrl(Url.REZULTATI_COM);
-	}
+    @Before
+    public void setUp() {
+        createChromeDriver();
+        openUrl(Url.REZULTATI_COM);
+    }
 
-	@Test public void matchStats() throws IOException, InterruptedException {
+    @Test
+    public void matchStats() throws IOException, InterruptedException {
 
-		Receiver receiver = new Receiver();
+        Receiver receiver = new Receiver();
+        receiver.collectMatch(Game.MINIMUM_ONE_GOAL_BOTH_HALF, 4);
+    }
 
-		//receiver.collectFullTimeMatch(Game.BOTH_TO_SCORE, 4);
-		receiver.collectHalfTimeMatch(Game.MINIMUM_ONE_GOAL_BOTH_HALF, 4);
-	}
-
-	@After public void close() {
-		quitDriver();
-	}
+    @After
+    public void close() {
+        quitDriver();
+    }
 }
